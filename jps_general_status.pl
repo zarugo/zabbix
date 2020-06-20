c2962de08c2eaf336534cc185f310a1a62aee244
zarugo <zar@pop-os.localdomain>
2020-06-20 15:18:23 +0200

    first commit of produciont env

---
+++ jps_general_status.pl
@@ -0,0 +1,14 @@
+for (`psql.exe -U jbl -d jbl -t -c "select (select count(*) from jps_peripheral_status jps,
+jps_authenticated_device jap
+where jps.peripheral_id = jap.peripheral_id
+and jps.status = 'Offline') as offline, count(*) as total
+from jps_peripheral_status jps,
+jps_authenticated_device jap
+where jps.peripheral_id = jap.peripheral_id" | sed 's/|//g'| head --lines=-1`)
+{
+($offlinejps, $totaljps) = m/(\S+)\s+(\S+)/;
+print "\t{\n";
+print "\t\t\"offline\":\"$offlinejps\",\n";
+print "\t\t\"total\":\"$totaljps\"\n";
+print "\t}\n";
+}
